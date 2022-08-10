fd145aed5d703f7699e14c4d51538cbbe028a692
GitHub Action <action@github.com>
2022-08-10 01:01:39 +0000

    Configure ezCatalog

--- public/pasta.js
+++ public/pasta.js
@@ -4,7 +4,7 @@
 
 var PASTA_CONFIG = {
    "server": "https://pasta.lternet.edu/package/search/eml?", // PASTA server
-   "filter": '&q=id:edi.400+id:edi.1137+id:edi.230+id:edi.713+id:edi.700+id:edi.398+id:edi.1137+id:edi.399+id:edi.405+id:edi.225', // Filter results on a unique keyword of a research group
+   "filter": '&q=id:edi.400+id:edi.1137+id:edi.230+id:edi.713+id:edi.700+id:edi.398+id:edi.1137+id:edi.399+id:edi.405+id:edi.225+id:edi.8675309+id:edi.1152+id:edi.1040', // Filter results on a unique keyword of a research group
    "limit": 10, // Max number of results to retrieve per page
    "resultsElementId": "searchResults", // Element to contain results
    "urlElementId": "searchUrl", // Element to display search URL. Use "searchUrl" to display or "" to remove FIXME: Empty string does not turn off.
